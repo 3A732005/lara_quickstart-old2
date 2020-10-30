f524d75eb1fb16b1555b22ad8209d6684fbaeb44
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-31 03:10:08 +0800

    +route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+
+Route::get('/', function () {
+    //
+});
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
