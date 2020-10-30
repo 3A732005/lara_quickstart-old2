977751592c6d2aae0f6234bde0627cf181b308f7
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-31 03:30:29 +0800

    第二個路由'/task'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,7 +27,18 @@ Route::get('/', function () {
 });
 
 Route::post('/task', function (Request $request) {
-    //
+    // 驗證輸入
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+//新增任務存入DB的程式碼 (see next page)
+
 });
 
 Route::delete('/task/{task}', function (Task $task) {
